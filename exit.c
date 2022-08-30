9769701692e36b413166b1aae5dfb9f8478a133b
Chinwendu Eke <170404060@live.unilag.edu.ng>
2022-08-30 10:23:17 -0700

    Exit only

--- exit.c
+++ exit.c
@@ -1,1 +1,12 @@
 #include "shell.h"
+
+/**
+ * sigintHandler - avoid ctrl + c
+ * @sig_num: value that received
+ * Return: nothing
+ */
+
+void sigintHandler(int sig_num __attribute__((unused)))
+{
+	write(1, "\nNew shell $ ", 13);
+}
